91b93d4fb4f05bd1ebc82f425f5a49e8a3396a33
patrykharasiuk <patryk.harasiuk@op.pl>
2022-09-04 14:28:49 +0200

    feat: add react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(() => new QueryClient());
+
+  <QueryClientProvider client={queryClient}>
+    <Hydrate state={pageProps.dehydratedState}>
+      <Component {...pageProps} />;
+    </Hydrate>
+    <ReactQueryDevtools />
+  </QueryClientProvider>;
 }
 
 export default MyApp;
