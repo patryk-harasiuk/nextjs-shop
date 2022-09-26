b0ea5a4e98660aaaebdd784702b8a0280b9971f0
patrykharasiuk <patryk.harasiuk@op.pl>
2022-09-26 16:34:11 +0200

    feat: add user and search icon

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -1,14 +1,16 @@
+import Image from 'next/image';
+
 import useAuth from '../../../hooks/useAuth/useAuth';
 
 const Header = () => {
   return (
     <header className="bg-slate-300 font-main">
       <div className="w-full flex justify-between items-center px-16 py-1">
-        <p className="text-xs">icon</p>
+        <Image src={'/user-icon.png'} alt="user" width="24px" height="24px" />
 
         <p className="text-xs">nextjs-shop</p>
 
-        <p className="text-xs">icon</p>
+        <Image src={'/search-icon.png'} alt="search" width="24px" height="24px" />
       </div>
     </header>
   );
