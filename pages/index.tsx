2981c7153a1d56f5bb26dc875580474d741b533a
patrykharasiuk <patryk.harasiuk@op.pl>
2022-10-01 17:49:31 +0200

    feat: remove unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import { signIn, useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 
 import Layout from '../components/layout/Layout';
 
@@ -7,23 +7,11 @@ const Home: NextPage = () => {
   const { data: session, status } = useSession();
 
   return (
-    // <div>
-    //   {!session && (
-    //     <>
-    //       <h1>You are not signed in</h1> <br />
-    //       <button onClick={() => signIn('github')}>Sign in</button>
-    //     </>
-    //   )}
-
-    //   {status === 'authenticated' && (
     <Layout>
       <div className="min-h-screen">
-        {/* <h1>Signed in as {session.user?.name} </h1> <br /> */}
         <h2>MAIN PAGE</h2>
       </div>
     </Layout>
-    //   )}
-    // </div>
   );
 };
 
